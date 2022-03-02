b9c1986f6500403fa1c2467d15d841b9dcd6ef2b
Michael Hansen <46607238+mhfalken@users.noreply.github.com>
2022-03-02 20:21:17 +0100

    Fix danish letters

--- pygame-start.py
+++ pygame-start.py
@@ -1,5 +1,5 @@
 # Standard control loop in pygame
-# Made by Michael Hansen 2021
+# Made by Michael Hansen 2021/22
 import pygame
 import os
 import random
@@ -31,14 +31,14 @@ def main():
 
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_SPACE:
-          pass    # Her er trykket på SPACE tasten
+          pass    # Her er trykket paa SPACE tasten
     keys_pressed = pygame.key.get_pressed()
-    if keys_pressed[pygame.K_LEFT]:
-      pass      # Her er trykket på VENSTRE-PILE tasten
+    if keys_pressed[pygame.K_RIGHT]:
+      ship.x += 1      # Her er trykket paa HOEJRE-PILE tast
 
     # Update display (WIN)
     WIN.fill(BLACK)
-    pygame.draw.rect(WIN, YELLOW, (ship.x, ship.y, ship.width, ship.height))  # Tegner den gule firkant
+    pygame.draw.rect(WIN, YELLOW, ship)  # Tegner den gule firkant
     pygame.display.update()
 
 
@@ -48,13 +48,16 @@ pygame.quit()
 
 
 
-
 """
 # Grafik
 SHIP_IMAGE = pygame.image.load('ship.png')
-SHIP_SCALE = pygame.transform.scale(SHIP_IMAGE, (20, 20))
-SHIP_ROTATE = pygame.transform.rotate(SHIP_SCALE, 90)
+SHIP_IMAGE = pygame.transform.scale(SHIP_IMAGE, (20, 20))
+SHIP_IMAGE = pygame.transform.rotate(SHIP_IMAGE, 90)
+SHIP_IMAGE = pygame.transform.flip(SHIP_IMAGE, True, True)
 WIN.blit(SHIP_IMAGE, ship)
+
+# Baggrundsbillede
+WIN.blit(BACKGROUND_IMAGE, (0, 0))
 
 # Tekst
 SCORE_FONT = pygame.font.SysFont('comicsans', 20)
@@ -69,15 +72,36 @@ HIT_SOUND.play()
 if ship.colliderect(alien):
   pass
 
+# Mus
+if event.type == pygame.MOUSEBUTTONDOWN:
+  (k1, k2, k3) = pygame.mouse.get_pressed()   # Museknapper: Venstre, midt, hoejre
+  (x, y) = pygame.mouse.get_pos()             # Curser position (x, y)
+  if k1 == True:
+    pass
+
 # Lister
 liste = []            # Lav en tom liste
-liste.append(item)    # Tilføj element
+liste.append(item)    # Tilfoej element
 liste.remove(item)    # Fjern element
 len(liste)            # Antal elementer i listen
-for item in liste:    # Løb listen igennem
+for item in liste:    # Loeb listen igennem
   pass
 
-# Tilfældigt tal
+# Class
+class EnemyClass:
+  speed = 1
+  def __init__(self, pygameRect, image):
+    self.rect = pygameRect   # pygame.Rect(...)
+    self.image = image       # pygame.image ...
+
+enemy = EnemyClass(pygame.Rect(200, 200, 20, 5), ENEMY_IMAGE)
+enemy.speed = 2
+
+pygame.draw.rect(WIN, RED, enemy.rect)  # Vis som firkant
+WIN.blit(enemy.image, enemy.rect)       # Vis som image
+
+
+# Tilfaeldigt tal
 random.randint(0, 100)  # [0-100]
 
 # Delay
@@ -87,3 +111,4 @@ pygame.time.delay(2000)  # Vent 2000 ms (2 sekunder)
 os.path.join('effects', 'ship.png')
 
 """
+
